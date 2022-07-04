fb26db80d285e2b1746e8214472f3b46df2de27c
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-07-03 20:52:12 -0500

    updating signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,20 @@
-const signupForm = async (event) => {
+async function signupFormHandler(event) {
     event.preventDefault();
     const name = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name, password) {
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ name, password }),
-            headers: { 'Content-Type': 'application/json' },
+    if (username && email && password) {
+        const response = await fetch("/api/users", {
+          method: "post",
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+          }),
         });
 
         if(response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else { alert('Failed to sign up!')}
     }
 };
