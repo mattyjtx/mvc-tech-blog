342e0a8982449cf63f08b6588ac6810aedfeb09e
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-07-03 20:23:47 -0500

    adjustments to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,22 +1,54 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+      const postData = await Post.findAll({
+          include: [
+              {
+                  model: User,
+                  attributes: ['name'],
+              },
+          ],
+          attributes: { exclude: ['password'] },
+      });
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render('homepage', {
+          posts,
+          logged_in: req.session.logged_in,
+          // logged_name comes trough sign in /with session so we can use user name with handlebars
+          logged_name: req.session.logged_name
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+  }
+});
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get('/posts/:id', withAuth, async (req,res) => {
+  try {
+      const postData = await Post.findByPk(req.params.id, {
+          include: [
+              {
+                  model: User,
+                  attributes: ['name'],
+              },
+              {
+                  model: Comment
+              },
+          ],
+      });
+      const post = postData.get({ plain:true });
+      res.render('single-post', {
+          ...post,
+          user_id: req.session.user_id,
+          logged_in: req.session.logged_in,
+          logged_name: req.session.logged_name
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
   }
 });
 
@@ -27,6 +59,14 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if(req.session.logged_in) {
+      res.redirect('/');
+      return;
+  }
+  res.render('signup');
 });
 
 module.exports = router;
